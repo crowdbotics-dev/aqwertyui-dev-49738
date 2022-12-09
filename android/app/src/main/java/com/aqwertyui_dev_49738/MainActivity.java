a8e937d1a9e6ea4283ff7c04b19a55637fa51905
Crowdbotics <engineering@crowdbotics.com>
2022-12-09 12:41:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/aqwertyui_dev_49738/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aqwertyui_dev_49738;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "aqwertyui_dev_49738";
+  }
+}
